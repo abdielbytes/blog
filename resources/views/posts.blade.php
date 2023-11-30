a751f76f6538092b8a4ced7aa9303d575daa338d
Wilsonabdiel <wilsonabdiel86@gmail.com>
2023-11-30 22:12:29 +0100

    feat:post.blade

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Posts</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1> <a href="post/first-post"> My First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cum cumque deleniti dignissimos dolore dolorem dolores eaque enim ex expedita impedit, molestiae mollitia officia omnis quibusdam totam voluptatum! Architecto, itaque.
+        </p>
+    </article>
+    <article>
+        <h1> <a href="post/second-post"> My Second Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cum cumque deleniti dignissimos dolore dolorem dolores eaque enim ex expedita impedit, molestiae mollitia officia omnis quibusdam totam voluptatum! Architecto, itaque.
+        </p>
+    </article>
+    <article>
+        <h1> <a href="post/third-post"> My third Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, aut, consequatur culpa deserunt doloremque ducimus enim expedita harum ipsum maiores minima neque odio porro quibusdam, ut. Alias reiciendis ullam vero?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cum cumque deleniti dignissimos dolore dolorem dolores eaque enim ex expedita impedit, molestiae mollitia officia omnis quibusdam totam voluptatum! Architecto, itaque.
+        </p>
+    </article>
+</body>
+</html>
